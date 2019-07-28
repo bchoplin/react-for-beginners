a5dc2bb9da2f06934e29c86c6be7558a346f90c7
Brittany Choplin <brittany@alley.co>
2019-07-28 17:55:54 -0400

    completed video 18

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -5,9 +5,13 @@ class Order extends React.Component {
 	renderOrder = (key) => {
 		const fish = this.props.fishes[key];
 		const count = this.props.order[key];
-		const isAvailable = fish.status === 'available';
-		if (!isAvailable) {
-			return <li key={key}>Sorry {fish ? fish.name : 'fish'} is no longer available</li>;
+		const isAvailable = fish && fish.status === 'available';
+		if(!isAvailable) {
+			return (
+				<li key={key}>
+					Sorry {fish ? fish.name : 'fish'} is no longer available
+				</li>
+			);
 		}
 		return (
 			<li key={key}>
